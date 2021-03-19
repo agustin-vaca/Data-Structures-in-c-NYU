258e0d2b7d26261cbc4a3e0d54cd6b3cdc0abd45
Agustin Vaca <agustin.vaca@outlook.com>
2021-03-19 16:22:36 -0400

    Commit for homework

--- csvimport.cpp
+++ csvimport.cpp
@@ -1,19 +1,28 @@
 #include <string>
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
 void csv_import(string data[][10], int columns, int *records, string filename) {
     std::ifstream inFile;
     inFile.open(filename);
+    string show, show2;
     for (int i = 0 ; i < *records; i++){
-        for (int j = 0; j < columns; j++){
-            if (j == columns - 1){
-                getline(inFile, data[i][j]);
-            }
-            else {
-                getline(inFile, data[i][j], ',');
-            }
-        }
+      inFile >> show;
+      for (int j = 0; j < columns; j++){
+        show2 = show.substr(j, show.find(','));
+        data[i][j] = show2;
+      }
     }
+}
+
+int main () {
+  string data[10][10];
+
+  int records;  
+
+  csv_import(data,3,&records,"text.csv");
+
+  cout << data[0][0];
 }
